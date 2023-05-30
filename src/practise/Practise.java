501f2087f8a94efb3440a6ff23cecb2f8ed2e2c8
Harleen kaur <Kau15021@shernet.sheridancollege.ca>
2023-05-30 14:27:38 -0400

    Version 1

---
+++ src/practise/Practise.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practise;
+
+/**
+ *
+ * @author harle
+ */
+public class Practise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is just a practise");
+    }
+    
+}
