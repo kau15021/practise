911c317adf00d290ea6f9438145e90024e4df3fd
Harleen kaur <Kau15021@shernet.sheridancollege.ca>
2023-05-30 14:47:24 -0400

    one more change has been made in the statement.

--- src/practise/Practise.java
+++ src/practise/Practise.java
@@ -14,7 +14,7 @@ public class Practise {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("This is just a practise for comit");
+        System.out.println("This is just a practise for comit haha");
     }
     
 }
